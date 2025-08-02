1eb79acde04d59b055b78a7251041caa3fc1bd07
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 09:45:40 +0100

    inputes were retrived and trimmed

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const feedbackDiv = document.getElementById('form-feedback');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
+const username = document.getElementById('username').value.trim();
+const email = document.getElementById('email').value.trim();
+const password = document.getElementById('password').value.trim();
   
 });
 });
