987738a65e21057316bd2a7af444721cb26243b1
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 09:39:43 +0100

    Form Selection and feedback added

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+   
 const form = document.getElementById('registration-form');
+const feedbackDiv = document.getElementById('form-feedback');
+});
