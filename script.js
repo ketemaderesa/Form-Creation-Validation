cc49a1f6f12ea0d2939368edc4e60958d637c114
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 09:42:27 +0100

    Submit Event Listene  added

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
    
 const form = document.getElementById('registration-form');
 const feedbackDiv = document.getElementById('form-feedback');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+  
+});
 });
