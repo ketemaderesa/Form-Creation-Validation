9471e0a8223a3809ec42027c8ff0bbcb34e0f36e
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 11:22:51 +0100

    Fetch data (try-catch block)

--- fetch-data.js
+++ fetch-data.js
@@ -2,4 +2,11 @@ async function fetchUserData() {
    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
    const dataContainer = document.getElementById('api-data');
     console.log("Container found:", dataContainer);
+     try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        console.log(users);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 }
